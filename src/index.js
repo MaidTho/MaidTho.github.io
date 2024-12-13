438e85bfa19cd9a3914b78f749d4ba5d9c27264e
MaidTho <tom.w.maidment@outlook.com>
2024-12-13 18:20:25 +1030

    • Update 2.0.0 React Integration + Grid Collage added.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
 import './index.css';
 import './contactform.css';
 import './restrictedtextarea.js';
