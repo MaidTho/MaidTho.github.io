bb6352331c55bcc7887a5339c53bc85934b44d1e
MaidTho <tom.w.maidment@outlook.com>
2024-12-22 21:50:02 +1030

    • Update 2.0.1 Dark Mode Contact!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 //import React from 'react';
 import logo from './Images/LogoRender.png';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> MadeToProject </p>
+        <h1> MadeToProject </h1>
       </header>
     </div>
   );
