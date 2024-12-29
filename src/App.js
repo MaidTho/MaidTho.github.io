9a157734dd31fa01d83558862e79d4ad8b3566ff
MaidTho <tom.w.maidment@outlook.com>
2024-12-29 20:13:55 +1030

    • Update 2.0.1 + 2.0.2.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@
 import logo from './Images/LogoRender.png';
 import './App.css';
 import './index.css';
+/*import './script.js';*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> MadeToProject </h1>
-      </header>
+        <canvas id="connected-dots"></canvas>
+        <script src="script.js"></script>  
+        <br></br>
+        <h1><b>MadeToProject</b></h1>
+        <h1>Hello I'm <span>Tom</span>!&#x1F44B;</h1>
+        <h2>I am a Frontend Software Engineer from <span>Adelaide</span>, South Australia!</h2>
+        <br></br>
+    </header>
     </div>
   );
 }
